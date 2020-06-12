c773e0887a6adcad9f6277e2380f39fb3864356f
Ravi <jravi248@gmail.com>
2020-06-12 14:31:19 +0530

    Replace nested if with switch for readability
    Signed-off-by: Ravi <jravi248@gmail.com>

--- scripts/timeSetter.js
+++ scripts/timeSetter.js
@@ -1,24 +1,30 @@
-const timeInputBox = document.getElementById('threshold-time');
-const timeSetButton = document.getElementById('time-button');
-const timeFormat = document.getElementById('format');
-const timeForm = document.getElementById('time-form');
+const timeInputBox = document.getElementById("threshold-time");
+const timeSetButton = document.getElementById("time-button");
+const timeFormat = document.getElementById("format");
+const timeForm = document.getElementById("time-form");
 
-timeSetButton.onclick = function(element) {
+timeSetButton.onclick = function (element) {
   var timeToSet;
 
   // Convert from given time format to seconds (should probably be using switch but I am lazy)
-  if(timeFormat.value === "Hours") 
-    timeToSet = timeInputBox.value * 60 * 60;
-  else if(timeFormat.value === "Days")
-    timeToSet = timeInputBox.value * 24 * 60 * 60;
-  else if(timeFormat.value === "Weeks")
-    timeToSet = timeInputBox.value * 7 * 24 * 60 * 60;
-  else if(timeFormat.value === "Minutes")
-    timeToSet = timeInputBox.value * 60;
-  else
-    timeToSet = timeInputBox.value;
+  switch (timeFormat.value) {
+    case "Hours":
+      timeToSet = timeInputBox.value * 60 * 60;
+      break;
+    case "Days":
+      timeToSet = timeInputBox.value * 24 * 60 * 60;
+      break;
+    case "Weeks":
+      timeToSet = timeInputBox.value * 7 * 24 * 60 * 60;
+      break;
+    case "Minutes":
+      timeToSet = timeInputBox.value * 60;
+      break;
+    default:
+      timeToSet = timeInputBox.value;
+  }
 
-  chrome.storage.sync.set({thresholdTime : timeToSet}, () => console.log(timeToSet));
+  chrome.storage.sync.set({ thresholdTime: timeToSet });
 };
 
-timeForm.addEventListener('submit', e => e.preventDefault());
+timeForm.addEventListener("submit", (e) => e.preventDefault());
